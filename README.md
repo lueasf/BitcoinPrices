cabd34fa1f0cd1d1df7971e92a178c2b7c8276fa
Lucie CORREIA <lucie.correia@telecomnancy.eu>
2025-02-13 00:47:56 +0100

    data prep

--- README.md
+++ README.md
@@ -30,3 +30,32 @@ pip install -r requirements.txt
 deactivate
 ```
 
+# Dataset
+
+## Fichiers
+sample_btcusd.csv est un fichier d'1 million de ligne qui sert de dataset, qui est tronqué d'un fichier de 6 millions de lignes qui contient des données sur 
+le bitcoin, à partir de 2012 et jusqu'à 2014 à CHAQUE SECONDES.
+
+À la base, la colone TimeStamp est de type UNIX time (nb de secondes, depuis le 1 Janvier 1970), on peut la convertir avec Pandas :
+- pd.to_datetime(df['TimeStamp'], unit='s')
+
+Pour réduire la taille, on utilise :
+- resample() pour moyenner les minutes en heures.
+- dropna() pour supprimer les valeurs manquantes.
+- reset_index() pour réinitialiser l'index.
+
+## Valeurs manquantes
+On peut utiliser:
+- isna(), qui est une méthode Pandas qui renvoie un masque booléen de la même forme que le DataFrame, indiquant les valeurs manquantes.
+Si on trouve une valeur manquante, on peut utiliser :
+- ffill() qui permet de remplir les valeurs manquantes avec la dernière valeur non manquante.
+
+## Data Prep
+On va performer un **Zero-shot learning**. On va faire une prédiction sans entrainer le modèle. 
+D'abord on va couper le dataset en :
+- Training set (80%)
+- Validation set (10%) permet d'ajuster le modèle.
+- Test set (10%)
+
+Puis on fera un **Fine-tuning** pour améliorer les performances.
+
--- app.py
+++ app.py
@@ -16,5 +16,58 @@ from tsfm_public.toolkit.time_series_forecasting_pipeline import TimeSeriesForec
 from tsfm_public.models.tinytimemixer import TinyTimeMixerForPrediction
 from tsfm_public.toolkit.callbacks import TrackingCallback
 
-bitcoin_data = pd.read_csv('btcusd-1-min-data.csv')
-bitcoin_data.head()
+from tsfm_public import (
+    TimeSeriesPreprocessor,
+    TinyTimeMixerForPrediction,
+    get_datasets,
+)
+
+bitcoin_data = pd.read_csv('sample_btcusd.csv')
+# print(len(bitcoin_data))
+# print(bitcoin_data.isna().sum())
+
+bitcoin_data['Timestamp'] = pd.to_datetime(bitcoin_data['Timestamp'], unit='s')
+# print(bitcoin_data.tail())
+
+# On va moyenner les données minutes par des données horaires
+bt_data_resampled = bitcoin_data.resample('H', on = 'Timestamp').mean().dropna().reset_index()
+# print(len(bt_data_resampled)) ~ 22 000, on a enlevé quasiment 100% des données
+
+print(f"Checking for no values after resampling:\n{bt_data_resampled.isna().sum()}\n")
+print(f"Number of entries after resampling: {len(bt_data_resampled)}")
+
+### Data Prep
+timestamp_column = 'TimeStamp'
+target_column = ['Close']
+observable_columns = ["Open", "High", "Low"]
+
+SEED = 42
+set_seed(SEED)
+
+### Paramètres de prévision (Forecasting Parameters = FP)
+context_length = 512
+forecast_length = 96 # prédire le prix dans 96 minutes en utilisant les 512 minutes précédentes!
+
+### Splitting the data
+data_lenght = len(bt_data_resampled)
+
+train_start_index = 0
+train_end_index = round(data_lenght * 0.8)
+
+# Validation set
+eval_start_index = round(data_lenght * 0.8) - context_length
+eval_end_index = round(data_lenght * 0.9) 
+
+#Test
+test_start_index = round(data_lenght * 0.9) - context_length
+test_end_index = data_lenght
+
+split_config = {
+    "train": [train_start_index, train_end_index],
+    "valid": [eval_start_index, eval_end_index],
+    "test": [test_start_index, test_end_index],
+}
+
+print(f"train_start_index: {train_start_index}, train_end_index: {train_end_index}")
+print(f"eval_start_index: {eval_start_index}, eval_end_index: {eval_end_index}")
+print(f"test_start_index: {test_start_index}, test_start_index: {test_end_index}")
